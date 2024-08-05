babbbacc619ae43c8f31826f607376c6a9e1a751
alejandroava <alejandroalzate.ava@gmail.com>
2024-08-05 06:37:17 -0500

    Feat: Se crea el molde base sin logica de la clase ManagerApp

---
+++ Models/ManagerApp.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaC_sharp_JonathanEscobarMolina.Models
+{
+    //Clase estatica
+    public static class ManagerApp
+    {
+        //Metodos
+        // public static Dog CreateDog(){
+
+        // }
+        // public static Cat CreateCat(){
+
+        // }
+
+        public static void ShowHeader(){
+
+        }
+        public static void ShowFooter(){
+
+        }
+        public static void ShowSeparator(){
+
+        }
+    }
+}
